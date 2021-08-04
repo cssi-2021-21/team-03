e9a467bdcc603c180e3b349a32348163e1b57b0e
Jessica Ruan <jessica.ruan@outlook.com>
2021-08-04 06:51:07 +0000

    add navbar, test create notif

--- public/js/components/progressView.jsx
+++ public/js/components/progressView.jsx
@@ -152,6 +152,22 @@ const deleteGoal = async (userId, goalId) => {
     }
 }
 
+// Create notif
+const createNotif = (content) => {
+    ReactDOM.render(<div class="notification is-info is-light">
+    <button class="delete"></button>
+        {content}
+    </div>, document.querySelector("#notif_container"));
+
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+
+}
+
 // Info for home icon
 const homeSVG = `M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z`;
 const checkDoubleSVG = `M505 174.8l-39.6-39.6c-9.4-9.4-24.6-9.4-33.9 0L192 374.7 80.6 263.2c-9.4-9.4-24.6-9.4-33.9 0L7 302.9c-9.4 9.4-9.4 24.6 0 34L175 505c9.4 9.4 24.6 9.4 33.9 0l296-296.2c9.4-9.5 9.4-24.7.1-34zm-324.3 106c6.2 6.3 16.4 6.3 22.6 0l208-208.2c6.2-6.3 6.2-16.4 0-22.6L366.1 4.7c-6.2-6.3-16.4-6.3-22.6 0L192 156.2l-55.4-55.5c-6.2-6.3-16.4-6.3-22.6 0L68.7 146c-6.2 6.3-6.2 16.4 0 22.6l112 112.2z`;
@@ -187,9 +203,9 @@ const pathVariants = {
 }
 
 // Render home icon
-ReactDOM.render(<a href="index.html">
+ReactDOM.render(<div class="navbar-brand">
+<a href="index.html" class="navbar-item">
     <motion.div
-        animate={{x:50, y:15}}
         whileHover={{fill: "#39cb75", originX: 0}}
         onMouseOver={toFancy}
         onMouseLeave={toSimple}>
@@ -201,7 +217,7 @@ ReactDOM.render(<a href="index.html">
                 <motion.path variants={pathVariants} id="tasks-icon" d={checkDoubleSVG}/>
             </motion.svg>
     </motion.div>
-</a>, document.querySelector('#animated_stuff_container'));
+</a></div>, document.querySelector('#animated_stuff_container'));
 
 
 // Render user progress
